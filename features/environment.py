a3dcfeb6e06a7dd8093dfed63c86a14f07541277
TChoi <thomasjung.choi@gmail.com>
2025-07-24 12:38:24 -0500

    task 5 internship allure reports

--- features/environment.py
+++ features/environment.py
@@ -1,12 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 import time
-from selenium.webdriver.firefox.service import Service
-from selenium.webdriver.firefox.options import Options
-from webdriver_manager.firefox import GeckoDriverManager
-from selenium.webdriver.edge.options import Options as EdgeOptions
+import allure
+from allure_commons.types import AttachmentType
 
 
 
@@ -14,47 +11,10 @@ def browser_init(context):
     """
     :param context: Behave context
     """
-
-
-    username = 'tomas_1aqxVc'
-    access_key = 'THbkesv3A4a47JA2HpxE'
-    browserstack_url = f"https://{username}:{access_key}@hub-cloud.browserstack.com/wd/hub"
-
-    options = EdgeOptions()
-
-    # Set browser name and version at top level
-    options.set_capability("browserName", "Edge")
-    options.set_capability("browserVersion", "latest")
-
-    # BrowserStack options inside 'bstack:options'
-    options.set_capability("bstack:options", {
-        "os": "Windows",
-        "osVersion": "11",
-        "sessionName": context.scenario.name if hasattr(context, 'scenario') else 'BDD Edge Test',
-        "debug": True,
-        "networkLogs": True,
-        "consoleLogs": "info"
-    })
-
-    context.driver = webdriver.Remote(
-        command_executor=browserstack_url,
-        options=options
-    )
-
-    #options = Options()
-    #options.add_argument("--headless")
-    #driver_path = GeckoDriverManager().install()
-    #driver_path = ChromeDriverManager().install()
-    #service = Service(driver_path)
-    #context.driver = webdriver.Firefox(service=service, options=options)
-    #context.driver = webdriver.Chrome(service=service, options=options)
-
-    #context.driver.maximize_window()
-
-    print(f"BrowserStack session started with ID: {context.driver.session_id}")
-
-
-    context.driver.set_window_size(1920, 1080)
+    driver_path = ChromeDriverManager().install()
+    service = Service(driver_path)
+    context.driver = webdriver.Chrome(service=service)
+    context.driver.maximize_window()
     context.driver.implicitly_wait(4)
 
 
@@ -71,6 +31,13 @@ def take_screenshot(context, name='screenshot'):
     filename = f"{name}_{timestamp}.png"
     context.driver.save_screenshot(filename)
     print(f"Screenshot saved as {filename}")
+
+    with open(filename, "rb") as image_file:
+        allure.attach(
+            image_file.read(),
+            name=name,
+            attachment_type=AttachmentType.PNG
+        )
 
 def after_step(context, step):
     if step.status == 'failed':
